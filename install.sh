91ff069ae8b5aae14f5ff8f0150295ee18e41e91
Reyoking <66134480+Reyoking@users.noreply.github.com>
2021-03-01 20:49:39 -0600

    Update install.sh
    Install a qpicospeaker.desktop shortcut

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 if [ $1 == "--uninstall" ]; then
     sudo rm /usr/bin/qpicospeaker
     rm -r ~/.config/qpicospeaker
+    sudo rm /usr/share/applications/qpicospeaker.desktop
+    sudo rm /usr/share/pixmaps/qpicospeaker.png
 else
     git submodule update --init --recursive
     rm -r ./bin
@@ -9,4 +11,7 @@ else
     qmake ../qpicospeaker.pro 
     make
     sudo mv qpicospeaker /usr/bin/
+    cd ..
+    sudo cp data/desktop/qpicospeaker.desktop /usr/share/applications/qpicospeaker.desktop
+    sudo cp data/icons/128x128/qpicospeaker.png /usr/share/pixmaps/qpicospeaker.png
 fi
